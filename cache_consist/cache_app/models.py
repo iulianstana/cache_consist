1d6fc9d0c54edd977a146224263368ea374442a6
Iulian <julian.stana@gmail.com>
2016-11-15 15:27:53 +0200

    [MODEL] create simple model that will serve my need

--- cache_consist/cache_app/models.py
+++ cache_consist/cache_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class TestRequest(models.Model):
+    status = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
