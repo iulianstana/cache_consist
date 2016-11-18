8b29e4457a3ae92edeee222e7968285b2017efa6
Iulian <julian.stana@gmail.com>
2016-11-18 13:41:09 +0200

    [unittest] new unittest that verify the common change

--- cache_consist/cache_app/cache_unittest.py
+++ cache_consist/cache_app/cache_unittest.py
@@ -48,3 +48,43 @@ class TestCache(unittest.TestCase):
         e = time.time()
         self.assertTrue(e - s > 4)
 
+    def test_common_data_changed(self):
+        """
+        get_result - no cache yet, it will take about 5 seconds
+        sleep 20 seconds
+        # tr_data expired
+        change tr_model
+        get_result - new tr_data, it will take about 5 seconds
+        sleep 30 seconds
+        get_result - use the preview cache.
+        """
+        # clean current cache
+        cache.clear()
+
+        # cache for 40 seconds with current tr_data
+        s = time.time()
+        cache_util.get_result_cache(1)
+        e = time.time()
+        self.assertTrue(e - s > 4)
+
+        # sleep and check the cache
+        time.sleep(20)
+
+        # change tr_model
+        tr_obj = models.TestRequest.objects.get(pk=1)
+        tr_obj.status = 'DONE'
+        tr_obj.save()
+
+        # new data cache for other 20 seconds
+        s = time.time()
+        cache_util.get_result_cache(1)
+        e = time.time()
+        self.assertTrue(e - s > 4)
+
+        # sleep and cache again
+        time.sleep(30)
+        s = time.time()
+        cache_util.get_result_cache(1)
+        e = time.time()
+        self.assertTrue(e - s < 1)
+
